c21d3b64a2777879a5b0b32077fc5ae1265ccf09
Yugapriya <yugapriya@rubixtek.com>
2023-06-23 13:36:01 +0530

    transform  object changes

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,33 +9,32 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material);
+const group=new THREE.Group();
+group.position.y=1;
+group.scale.y=2;
+group.rotation.y=1;
+scene.add(group);
 
-//Positioning
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-scene.add(mesh)
+const cube1=new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0xff0000})
+);
+group.add(cube1);
+const cube2=new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0x0ff000})
+);
+cube2.position.x=-2;
+group.add(cube2);
+const cube3=new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0x0000ff})
+);
+cube3.position.x=2;
+group.add(cube3);
 
-mesh.position.set(0.7,-0.6,1)
 
-// Scale
-// mesh.scale.x=2;
-// mesh.scale.y=0.5;
-// mesh.scale.z=0.5;
 
-mesh.scale.set(2,0.5,0.5);
-
-// Rotation
-mesh.rotation.reorder('YXZ');
-mesh.rotation.x=Math.PI*0.25;
-mesh.rotation.y=Math.PI*0.25;
-
-console.log(mesh.position.length())
-// mesh.position.normalize()
-// console.log(mesh.position.length())
 
 /**
  * Axes Helper
@@ -62,9 +61,7 @@ camera.position.z = 3
 // camera.position.x = 1
 scene.add(camera)
 
-camera.lookAt(mesh.position)
 
-console.log(mesh.position.distanceTo(camera.position))
 /**
  * Renderer
  */
