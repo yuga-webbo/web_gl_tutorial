3ef27ee3b4bedad781e8fbb2fba24fa8e7c5b7a6
Yugapriya <yugapriya@rubixtek.com>
2023-06-23 12:55:02 +0530

    basic changes

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -11,8 +11,39 @@ const scene = new THREE.Scene()
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const mesh = new THREE.Mesh(geometry, material);
+
+//Positioning
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
 scene.add(mesh)
+
+mesh.position.set(0.7,-0.6,1)
+
+// Scale
+// mesh.scale.x=2;
+// mesh.scale.y=0.5;
+// mesh.scale.z=0.5;
+
+mesh.scale.set(2,0.5,0.5);
+
+// Rotation
+mesh.rotation.reorder('YXZ');
+mesh.rotation.x=Math.PI*0.25;
+mesh.rotation.y=Math.PI*0.25;
+
+console.log(mesh.position.length())
+// mesh.position.normalize()
+// console.log(mesh.position.length())
+
+/**
+ * Axes Helper
+ */
+const axesHelper=new THREE.AxesHelper()
+scene.add(axesHelper)
+
+
 
 /**
  * Sizes
@@ -27,8 +58,13 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// camera.position.y = 1
+// camera.position.x = 1
 scene.add(camera)
 
+camera.lookAt(mesh.position)
+
+console.log(mesh.position.distanceTo(camera.position))
 /**
  * Renderer
  */
