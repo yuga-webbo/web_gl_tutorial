89f4ee03009b83f8ec1c04eb9ea42438be4e1a01
Yugapriya <yugapriya@rubixtek.com>
2023-06-23 14:01:50 +0530

    Time calculation

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -28,4 +28,23 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+//Time
+let time=Date.now();
+
+// Animation
+const tick=()=>{
+    //Time
+    const currentTime=Date.now();
+    const deltaTime=currentTime-time;
+    time=currentTime;    
+
+    //Update Objects
+    // mesh.position.x+=0.01;
+    // mesh.position.y+=0.01;
+    mesh.rotation.y += 0.001 * deltaTime;
+
+    //Renderer
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
+}
+tick();
