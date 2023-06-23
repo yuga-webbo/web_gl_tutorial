2a27ac67f0111ddf7ac479afa6597f5b275e64c2
Yugapriya <yugapriya@rubixtek.com>
2023-06-23 15:59:22 +0530

    cursor control added

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,4 +1,15 @@
 import * as THREE from 'three'
+/**
+ * Cursor
+ */
+const cursor={
+    x:0,y:0
+}
+window.addEventListener('mousemove', (event) =>{
+    cursor.x=event.clientX/sizes.width-0.5
+    cursor.y=(event.clientY/sizes.height-0.5)
+});
+
 
 /**
  * Base
@@ -23,9 +34,11 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height,0.1,100)
+// const aspectRatio=sizes.width/sizes.height;
+//const camera=new THREE.OrthographicCamera(-1*aspectRatio,1*aspectRatio,1,-1,0.1,100)
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
@@ -44,7 +57,12 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+
+    // Update Camera
+    camera.position.x=cursor.x*10;
+    camera.position.y=cursor.y*10;
+    camera.lookAt(mesh.position);
 
     // Render
     renderer.render(scene, camera)
