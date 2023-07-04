9229064a2dd9a2aa108f84e31f4550d556140d31
Yugapriya <yugapriya@rubixtek.com>
2023-07-04 12:25:54 +0530

    full screen chnages added

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -41,6 +41,23 @@ console.log('resize')
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))  
 
 });
+window.addEventListener('dblclick', () =>{
+    console.log("dblclick")
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+    if(!fullscreenElement){
+        if(canvas.requestFullscreen){
+            canvas.requestFullscreen();
+        }else if(canvas.webkitRequestFullscreen){
+            canvas.webkitRequestFullscreen();
+        }
+    }else{
+        if(document.exitFullscreen){
+            document.exitFullscreen();
+        }else if(document.webkitExitFullscreen){
+            document.webkitExitFullscreen();
+        }
+    }
+});
 
 /**
  * Camera
